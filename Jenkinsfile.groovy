a9aa59b3d2f07bf30aa5f7c8a6958a24e89f721e
rameca231190 <romapereverziev@gmail.com>
2019-03-20 18:49:34 -0500

    added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,18 +1,18 @@
 node {
     properties([parameters([string(defaultValue: '127.0.0.1', description: '', name: 'IP', trim: true)])])
     stage("Install git"){
-        sh 'ssh ec2-user@IP     sudo yum install git python-pip -y'
+        sh "ssh ec2-user@${IP}     sudo yum install git python-pip -y"
     }
     stage("Clone a repo"){
         git 'https://github.com/rameca231190/flask-examples.git'
     }
     stage("Copy files"){
-        sh "scp * wc2-user@IP:/tmp/"
+        sh "scp * wc2-user@${IP}:/tmp/"
     }
     stage("Install requirements"){
-        sh "ssh ec2-user@IP    sudo pip install -t /tmp/requirements.txt"
+        sh "ssh ec2-user@${IP}    sudo pip install -t /tmp/requirements.txt"
     }
     stage("Run App"){
-        sh 'ssh ec2-user@IP    python /tmp/01-hello-world/hello.py'
+        sh "ssh ec2-user@${IP}    python /tmp/01-hello-world/hello.py"
     }
 }
